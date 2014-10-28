dab1b2a51cca4cccdd0c31a7acfe2b350d776767
Luis Alejandro <l.alejandro.gb@gmail.com>
2014-10-28 12:21:11 +0100

    Añadidos los archivos base
    Se han añadido los archivos del ejemplo ColorBasics como punto de
    partida para la práctica 2

---
+++ ejercicio_fitness.cs
@@ -0,0 +1,18 @@
+﻿namespace Microsoft.Samples.Kinect.ColorBasics
+{
+    using System;
+    using System.Globalization;
+    using System.IO;
+    using System.Windows;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using Microsoft.Kinect;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+   public partial class MainWindow : Window
+   {
+
+   }
+}
